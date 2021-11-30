b09f4149c8bb15dc90a180760a26a6054923a121
Yujin <yujinyll9148@gmail.com>
2021-11-30 15:23:39 +0900

    [feat] maincontainer 컴포넌트 추가

---
+++ src/components/about/MainContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledMainContainer = styled.div`
+  max-width: 1280px;
+  margin: 0 auto;
+`;
+
+function MainContainer({ ...rest }) {
+  return <StyledMainContainer {...rest}></StyledMainContainer>;
+}
+
+export default MainContainer;
