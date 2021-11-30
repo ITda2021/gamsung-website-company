93f91cb93dafd9a59b684fe58224056875d62f66
Yujin <yujinyll9148@gmail.com>
2021-11-30 15:22:21 +0900

    [feat] caption 컴포넌트 추가

---
+++ src/components/about/Caption.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+import palette from "lib/style/palette";
+
+const StyledCaption = styled.div`
+  font-family: Montserrat;
+  font-weight: 600;
+  font-size: 1.6rem;
+  letter-spacing: 0.355em;
+  color: ${(props) => props.color || palette.gray[3]};
+`;
+
+function Caption({ children, ...rest }) {
+  return <StyledCaption {...rest}>{children}</StyledCaption>;
+}
+
+export default Caption;
